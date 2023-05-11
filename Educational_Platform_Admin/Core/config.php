27ef2d088ebb1cdb3cd35cdf109d100652c396f9
omar saad <106129498+testt-101@users.noreply.github.com>
2023-05-11 21:33:30 +0300

    Delete Educational_Platform_Admin directory

--- Educational_Platform_Admin/Core/config.php
+++
@@ -1,9 +0,0 @@
-<?php
- 
-$databasename = "Educational metaverse platform";
-$host = "localhost";
-$dbuser = "root";
-$dbpassword = "";
-$connection = mysqli_connect($host, $dbuser, $dbpassword, $databasename);
- 
-?>
